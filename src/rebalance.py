6806abc066aaca70104f152aa9b04904741a725a
Floyd Berndsen <floyd.berndsen@gmail.com>
2021-04-04 19:15:04 +0200

    added first example with data

--- src/rebalance.py
+++ src/rebalance.py
@@ -1,11 +1,47 @@
-# what's necessary for rebalancing?
-# Quantity of holdings, value of holdings, calculated: current distribution
-# Strategy: weight per stock, required movement of distribution
-# Toggleable feature: fractional shares or no
+from pathlib import Path
 
-# func 1: get csv with holding id, quantity, price > output: dict/np matrix?
-# func 2: calculate current distribution
-# func 3: get current price per share
-# variant 1: ask user to input prices
-# variant 2 (later): get from api
-# func 4: calculate target distribution
+import numpy as np
+import pandas as pd
+
+
+def current_allocation(holdings: pd.DataFrame):
+    df = pd.DataFrame()
+    df["stock"] = holdings["stock"]
+    df["price"] = holdings["price"]
+    df["total"] = holdings["quantity"] * holdings["price"]
+    df["pct_of_total"] = df["total"] / sum(df["total"])
+    return df
+
+
+def rebalance(current_allocation: pd.DataFrame, target_allocation: dict):
+    """Compares current allocation to target allocation,
+    calculates which stocks to buy and sell to achieve target allocation"""
+    df = pd.DataFrame()
+    df["stock"] = current_allocation["stock"]
+    df["price"] = current_allocation["price"]
+    df["total"] = current_allocation["total"]
+    df["target"] = sum(current_allocation["total"]) * current_allocation["stock"].map(
+        target_allocation
+    )
+    df["difference"] = df["total"] - df["target"]
+    df["buy"] = df[["difference", "price"]].apply(
+        lambda x: x["difference"] / x["price"] if x["difference"] > 0 else 0, axis=1
+    )
+    df["sell"] = df[["difference", "price"]].apply(
+        lambda x: x["difference"] / x["price"] * -1 if x["difference"] < 0 else 0,
+        axis=1,
+    )
+    return df
+
+
+if __name__ == "__main__":
+    file = Path.cwd() / "data" / "example_data.csv"
+    data = pd.read_csv(file)
+    target_allocation = {"A": 0.25, "B": 0.25, "C": 0.25, "D": 0.25}
+
+    print(
+        rebalance(
+            current_allocation=current_allocation(data),
+            target_allocation=target_allocation,
+        )
+    )
