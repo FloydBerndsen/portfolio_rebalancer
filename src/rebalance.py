fd6ff7aff3bd7d94fe19eb0f4b97607a0220b81a
Floyd Berndsen <floyd.berndsen@gmail.com>
2021-04-04 17:11:04 +0200

    init repo

---
+++ src/rebalance.py
@@ -0,0 +1,11 @@
+# what's necessary for rebalancing?
+# Quantity of holdings, value of holdings, calculated: current distribution
+# Strategy: weight per stock, required movement of distribution
+# Toggleable feature: fractional shares or no
+
+# func 1: get csv with holding id, quantity, price > output: dict/np matrix?
+# func 2: calculate current distribution
+# func 3: get current price per share
+# variant 1: ask user to input prices
+# variant 2 (later): get from api
+# func 4: calculate target distribution
